106e89b6c45010600ca5a22a6cc38bcdc21f5c49
Valentin <ValentinVoznyuk@gmail.com>
2021-08-27 14:05:31 +0300

    fix bug

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
 import { FC } from "react";
 import render, { css } from 'reshadow';
 
-const styles = css`Layout { background-color: green }`;
+const styles = css`layout { background-color: green }`;
 
 export const Layout: FC = () => render(styles)(<layout as="p">Hello world!</layout>);
